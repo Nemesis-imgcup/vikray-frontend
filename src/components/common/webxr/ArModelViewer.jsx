073ad1c044264523e3bf6306aa2a8a37e6fd6645
kriptonian <shymal.sawan@gmail.com>
2022-12-24 02:47:11 +0530

    feat: add custom view ar btn, back btn, and hotspot

--- src/components/common/webxr/ArModelViewer.jsx
+++ src/components/common/webxr/ArModelViewer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import nike_air_zoom_pegasus_36 from './nike_air_zoom_pegasus_36.glb'
 import envLight from "./studio_small_09_2k.hdr"
-// import model-viewer package from node_modules
 import '@google/model-viewer'
 import { ArIcon2 } from './assets'
+import "./style/ArModelViewer.css"
 
 const ArModelViewer = () => {
     return (
@@ -24,18 +24,39 @@ const ArModelViewer = () => {
                     <div className="update-bar"></div>
                 </div> */}
 
+                {/* Hotspot */}
+                <div class="Hotspot" className='flex items-center space-x-2' slot="hotspot-1" data-position="-0.1063310426762897m 0.20027422684293655m -0.08258775633216421m" data-normal="-0.26661702648359775m 0.806294572785741m -0.5280193396887126m" data-visibility-attribute="visible">
+
+                    <button class="annotation" data-visibility-attribute="visible" className='w-[20px] h-[20px] border border-white bg-[#ffffff9a] rounded-full'></button>
+                    <div className='pl-2 absolute translate-x-6 w-[10rem] felx items-center text-white backdrop-blur-md border border-white p-1 rounded-full'>
+                        Strong Lace
+                    </div>
+                </div>
 
 
-                <button className='text-white mt-[50vh] mx-[27vw] flex justify-center w-[12rem] items-center space-x-3 border border-white p-2 rounded-full hover:bg-[#ffffff2c]' slot="ar-button"  id="ar-button">
+
+                {/* Button to trigger AR */}
+                <button className='text-white mt-[50vh] mx-[27vw] flex backdrop-blur-lg justify-center w-[12rem] items-center space-x-3 border border-white p-2 rounded-full hover:bg-[#ffffff2c]' slot="ar-button" id="ar-button">
                     <div id="ar-prompt">
-                        {/* <ArIcon /> */}
                         <ArIcon2 />
                     </div>
                     <div className='text-sm'>
                         View in your space
                     </div>
-
                 </button>
+
+                {/* Back button */}
+                <button slot="exit-webxr-ar-button" className="mt-[5vh] ml-[2vh]" >
+                    <div className='w-[40px] h-[40px] flex justify-center items-center border-[3px] border-white rounded-full'>
+                        <div className='pb-1'>
+                            <div className='w-[13px] rotate-[140deg] rounded-full h-[4px] bg-white'></div>
+                            <div className='w-[16px] h-[3px] bg-transparent rounded-full'></div>
+                            <div className='w-[13px] absolute rotate-[40deg] rounded-full h-[4px] bg-white'></div>
+                        </div>
+                    </div>
+                </button>
+
+
 
 
             </model-viewer>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import { ArIcon } from '../common/webxr/assets'
+import { ArIcon, ExitArBtnIcon } from '../common/webxr/assets'
 
 const Home = () => {
     return (
         <div className='bg-black h-[100vh] p-10'>Home
-            <button className='text-white flex items-center space-x-3 border border-white p-2 rounded-full hover:bg-[#ffffff2c]' slot="ar-button" id="ar-button">
-                <ArIcon />
-                <div className='text-sm'>
-                    View in your space
-                </div>
-            </button>
+            <ExitArBtnIcon />
         </div>
     )
 }
