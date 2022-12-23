3859b11fb2a630991afd01895a106fb7809dcef9
kriptonian <shymal.sawan@gmail.com>
2022-12-23 23:27:38 +0530

    feat: add AR model viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { ArModelViewer } from "./components/common";
 import { InternalServerError, PageNotFound } from "./components/Error";
 import Home from "./components/home/Home";
 
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/ise" element={<InternalServerError/>} />
+      <Route path="/arTest" element={<ArModelViewer/>} />
       <Route path="*" element={<PageNotFound/>} />
     </Routes>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { ArIcon } from '../common/webxr/assets'
 
 const Home = () => {
     return (
-        <div className=''>Home</div>
+        <div className='bg-black h-[100vh] p-10'>Home
+            <button className='text-white flex items-center space-x-3 border border-white p-2 rounded-full hover:bg-[#ffffff2c]' slot="ar-button" id="ar-button">
+                <ArIcon />
+                <div className='text-sm'>
+                    View in your space
+                </div>
+            </button>
+        </div>
     )
 }
 
