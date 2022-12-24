95fc6b6b5b19be8b4f048ee91ed7deb26bbbb085
Raj Saha <rajsaha20026@gmail.com>
2022-12-24 18:44:19 +0530

    created the front page or home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Navbar } from '../common';
 import "./home.css";
+import '../common/buttons/button.css'
+
 
 
 const Home = () => {
     return (
-        <div className='tom'>
+        <div className='home'>
             <Navbar/>
-            <h1>home</h1>
+            <h1>With buying your favorite<span> Goodies</span><br></br>
+             You can <span>mint</span> your own <span>NFTs</span>
+            </h1>
+            <p>Online metaverse shopping store to buy all your physical and digital goodies<br></br> and assets together with more user focused features.</p>
+              
+    <div class= "container">
+      <a href="#" class="neon-button orange">Lets shop</a>
+    </div>
+            <div class="line-1"></div>
+            <div class='meta'><h2>METAVERSE    SHOPPING</h2></div>
+            <div class="line-2"></div>
             </div>
+            
+            
     )
 }
 
