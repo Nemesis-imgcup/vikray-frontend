cfcbd361c4e82161213156121d40fcd51d828d4b
Raj Saha <rajsaha20026@gmail.com>
2022-12-24 12:36:54 +0530

    feat: add navbar

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavBar } from '../common/Navbar';
 import "./home.css";
+
 
 const Home = () => {
     return (
-        <div className='tom'><h1>home</h1></div>
+        <div className='tom'>
+            <NavBar/>
+            <h1>home</h1>
+            </div>
     )
 }
 
