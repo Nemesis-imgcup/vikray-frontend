6c008eb031efc688deb5ac01500afc6f0097a334
Arindam Sahoo <arindams2002@gmail.com>
2022-12-27 19:19:05 +0530

    Fixing Push

--- src/components/shop/shopComponents/TopPicksForYou.jsx
+++ src/components/shop/shopComponents/TopPicksForYou.jsx
@@ -31,15 +31,9 @@ const TopPicksForYou = () => {
     ]
 
     return (
-<<<<<<< Updated upstream
-        <div>
-             <div class="flex">
-                <div className="flex justify-left text-white font-medium text-2xl pl-5 pt-[15vh]">
-=======
         <div  className='flex flex-col mx-[10vw]'>
             <div class="flex">
                 <div className="flex justify-left text-white font-medium text-2xl pt-[15vh]">
->>>>>>> Stashed changes
                     TOP PICKS FOR YOU
                 </div>
                 <div class="flex border h-0 w-[10vw] border-white mt-[17vh] ml-[1em]"></div>
